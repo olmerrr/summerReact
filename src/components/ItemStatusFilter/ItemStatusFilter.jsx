840f2eaea775397c27c0249a815c5657f53cb675
Yaroslav <dombrovskiy91@gmail.com>
2020-08-10 16:20:46 +0300

    func component to class component

--- src/components/ItemStatusFilter/ItemStatusFilter.jsx
+++ src/components/ItemStatusFilter/ItemStatusFilter.jsx
@@ -6,6 +6,7 @@ export default class  ItemStatusFilter extends Component{
         <div className="btn-group">
             <button type="button" className="btn btn-info">All</button>
             <button type="button" className="btn btn-outline-secondary">Active</button>
+
             <button type="button" className="btn btn-outline-secondary">Done</button>
         </div>
 );

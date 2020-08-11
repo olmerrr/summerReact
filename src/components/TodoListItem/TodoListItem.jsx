7f243c13931a58cd44a2e39f2a8001058f9d1ef7
Yaroslav <dombrovskiy91@gmail.com>
2020-08-11 19:16:38 +0300

    add setState()+

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -8,13 +8,17 @@ export default class TodoListItem extends Component{
         important: false
     }
     onLabelClick = () => {
-           this.setState({
-               done: true
+           this.setState(({done}) =>{
+               return{
+                   done: !done
+               };
            });
         };
     onMarkImportant = () => {
-        this.setState({
-            important: true
+        this.setState(({important}) => {
+            return {
+                important: !important
+            };
         });
     };
 
